91befd30418f63cff3cd0f45a4b77d333b4c2abb
hemachandran9944 <hemachandranhema8754@gmail.com>
2026-03-25 13:00:57 +0530

    Backend connection success and verified local

--- blog/backend/server.js
+++ blog/backend/server.js
@@ -26,10 +26,17 @@ app.use(bodyParser.json()) ;
 // Connect to MongoDB using Environment Variable 
 
 //const mongoURI = process.env.MONGO_URI;
-const mongoURI = process.env.MONGO_URI || 'mongodb://hemachandranhema8754_db_user:F09oZcrRvHjg9hvj@ac-cdvwql2-shard-00-00.62bc372.mongodb.net:27017,ac-cdvwql2-shard-01.62bc372.mongodb.net:27017,ac-cdvwql2-shard-02.62bc372.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmbi4s-shard-0&authSource=admin&appName=Cluster0';
+
+const mongoURI = "mongodb://hemachandranhema8754_db_user:F09oZcrRvHjg9hvj@ac-cduwql2-shard-00-00.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-01.62bc372.mongodb.net:27017,ac-cduwql2-shard-00-02.62bc372.mongodb.net:27017/Hema_DB?ssl=true&replicaSet=atlas-fmbi4s-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB Connected to Hema_DB!'))
-  .catch(err => console.error('DB Connection Error:', err));
+  .then(() => {
+    console.log(' DataBase Connected Success ');
+    console.log('MongoDB Connected to Hema_DB!');
+  })
+  .catch(err => {
+    console.log('--- DB Connection Error ---');
+    console.error(err);
+  });
 //User router
 
 app.use('/api/posts', postRoutes)
