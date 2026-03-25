3da076f696127c51a4e3e178a2f805cb2abce00c
hemachandran9944 <hemachandranhema8754@gmail.com>
2026-03-25 11:08:39 +0530

    Fixing Bug

--- blog/backend/server.js
+++ blog/backend/server.js
@@ -25,14 +25,11 @@ app.use(bodyParser.json()) ;
 
 // Connect to MongoDB using Environment Variable 
 
-const mongoURI = process.env.MONGO_URI;
+//const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI || 'mongodb://hemachandranhema8754_db_user:F09oZcrRvHjg9hvj@ac-cdvwql2-shard-00-00.62bc372.mongodb.net:27017,ac-cdvwql2-shard-01.62bc372.mongodb.net:27017,ac-cdvwql2-shard-02.62bc372.mongodb.net:27017/?ssl=true&replicaSet=atlas-fmbi4s-shard-0&authSource=admin&appName=Cluster0';
 mongoose.connect(mongoURI)
- .then(() => console.log('MongoDB Connected to Hema_DB!'))
- .catch(err => {
-    console.log('DB Connect Error :');
-    console.error(err);
- });
-
+  .then(() => console.log('MongoDB Connected to Hema_DB!'))
+  .catch(err => console.error('DB Connection Error:', err));
 //User router
 
 app.use('/api/posts', postRoutes)
